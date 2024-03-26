74e9d3d77a12727ab456daf72b25377f7065b73a
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-03-26 15:52:42 +0400

    udpate

---
+++ 25.03.2024/app.js
@@ -0,0 +1,13 @@
+let num1 = +prompt("birinci ededi daxil edin");
+let num2 = +prompt("ikinci ededi daxil edin");
+let sum = num1 + num2;
+
+if (sum % 2 === 0) {
+  console.log(
+    ` ${num1} ve ${num2} ededlerinin cemi ${sum} ve  ${sum} cut ededdir`
+  );
+} else {
+  console.log(
+    ` ${num1} ve ${num2} ededlerinin cemi ${sum} ve  ${sum} tek ededdir`
+  );
+}
